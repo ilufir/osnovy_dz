6e3c849c7c2e897fbfa3c1811ac1df82366c4549
ilufir <54719011+ilufir@users.noreply.github.com>
2019-11-22 21:04:09 +0300

    Add files via upload

---
+++ blok1/b1t6main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float g = 9.8;
+    int r,h;
+    scanf("%d%d",&r,&h);
+    float p = r*g*h;
+    printf("P = %f na",p );
+    return 0;
+}
